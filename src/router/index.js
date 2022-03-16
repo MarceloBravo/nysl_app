c172e1b9bf339e0558fa0c71e36ca69379bde2dd
Marcelo <mabc@live.cl>
2022-03-16 18:34:09 -0300

    Se crearon las pantallas Home, About, GamesInfo y Contact

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/HomeView/HomeView.vue'
+import AboutView from '../views/AboutView/AboutView.vue'
+import GamesInformationView from '../views/GamesInformationView/GamesInformationView.vue'
+import ContactView from '../views/ContactView/ContactView.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  { path: '/', name: 'home', component: HomeView },
+  { path: '/about', name: 'about', component: AboutView },
+  { path: '/game-info', name: 'games-information', component: GamesInformationView },
+  { path: '/contact', name: 'contact', component: ContactView }
 ]
 
 const router = createRouter({
---
+++ src/views/HomeView/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <img class="main-image" src="../../assets/nysl_logo.png" alt="NYSL">
+    </div>
+</template>
+
+<style>
+    @import './HomeView.scss';
+</style>
